8e860a22791888314ab60ac7f04e6a90d7e6602d
Peaches_MLG <PeachesMLG100@gmail.com>
2019-07-06 19:20:05 +0100

    Fixed a typo in the config

--- src/main/java/com/peaches/epicskyblock/configs/Config.java
+++ src/main/java/com/peaches/epicskyblock/configs/Config.java
@@ -6,7 +6,7 @@ public class Config {
 
     public String prefix = "&e&lEpicSkyblock";
     public boolean sendErrorReports = true;
-    public int distance = 20;
+    public int distance = 200;
     public int size = 50;
     public Biome defaultBiome = Biome.PLAINS;
 
